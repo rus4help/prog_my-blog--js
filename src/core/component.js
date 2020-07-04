8689ec165fa8e9c5c7256f11e57bb3bf0fc6fc56
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-04 15:03:36 +0300

    Закончил реализацию header.component

--- src/core/component.js
+++ src/core/component.js
@@ -1,5 +1,16 @@
-class Component {
+export class Component {
     constructor(id) {
         this.$el = document.getElementById(id)
+        this.init()
+    }
+
+    init() {}
+
+    hide() {
+        this.$el.classList.add('hide')
+    }
+
+    show() {
+        this.$el.classList.remove('hide')
     }
 }
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import {HeaderComponent} from './components/header.copmonent'
+
+const header = new HeaderComponent('header')
+
+console.log(header);
