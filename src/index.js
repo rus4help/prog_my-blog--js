b30f50acde85017797037b602795d87eb662cb97
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-05 11:09:39 +0300

    Создаю отдельные классы для навигации

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { HeaderComponent } from './components/header.copmonent'
+import { NavigationComponent } from './components/navigation.component';
 
 new HeaderComponent('header')
 
-
+const navigation = new NavigationComponent('navigation')
