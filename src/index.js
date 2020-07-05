66b2e93d3e2152c97efd60f779effcb6017d6e19
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-05 11:26:30 +0300

    Подключил компоненты в главный файл

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { HeaderComponent } from './components/header.copmonent'
 import { NavigationComponent } from './components/navigation.component';
+import { CreateComponent } from './components/create.component';
+import { FavoriteComponent } from './components/favorite.component';
+import { PostsComponent } from './components/posts.component';
 
 new HeaderComponent('header')
 
 const navigation = new NavigationComponent('navigation')
+const create = new NavigationComponent('create')
+const favorite = new NavigationComponent('favorite')
+const posts = new NavigationComponent('posts')
