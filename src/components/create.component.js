4fc40af049e8331ff98fa203e789432cc850b33a
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-06 16:10:46 +0300

    Обрабатываю форму

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,7 +1,23 @@
-import { Component } from '../core/component';
+import { Component } from '../core/component'
+import { Form } from '../core/form'
 
 export class CreateComponent extends Component {
     constructor(id) {
         super(id)
+
+        this.form = null
     }
+
+    init() {
+        this.$el.addEventListener('submit', submitHandler.bind(this))
+
+        this.form = new Form()
+    }
+}
+
+function submitHandler(event) {
+    event.preventDefault()
+
+    console.log('submit');
+
 }
