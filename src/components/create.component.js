36b8290e868f195946e1022c422b4d3b53858994
Denis Nikhaev <denis.nikhaev@gmail.com>
2020-07-06 17:30:07 +0300

    Валидация форм настроена

--- src/components/create.component.js
+++ src/components/create.component.js
@@ -1,5 +1,6 @@
 import { Component } from '../core/component'
 import { Form } from '../core/form'
+import { Validators } from '../core/validators'
 
 export class CreateComponent extends Component {
     constructor(id) {
@@ -10,8 +11,8 @@ export class CreateComponent extends Component {
         this.$el.addEventListener('submit', submitHandler.bind(this))
 
         this.form = new Form(this.$el, {
-            title: [],
-            fulltext: []
+            title: [Validators.required],
+            fulltext: [Validators.required, Validators.minLength(10)]
         })
     }
 }
@@ -19,11 +20,18 @@ export class CreateComponent extends Component {
 function submitHandler(event) {
     event.preventDefault()
 
-    const formData = {
-        type: this.$el.type.value,
-        ...this.form.value()
+    if (this.form.isValid()) {
+        const formData = {
+            type: this.$el.type.value,
+            ...this.form.value()
+        }
+
+        console.log('submit', formData)
+    } else {
+        console.warn('Form is invalid');
+
     }
 
-    console.log('submit', formData);
+
 
 }
